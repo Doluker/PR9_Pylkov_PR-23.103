fd01ac2c4ff31c333cf8f1339e1c25866c5fb874
Doluker <kakafka1842@gmail.com>
2025-12-04 00:42:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr9/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pr9;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickStepsActivity(View view) {
+        Intent intent = new Intent(this, AlbumsActivity.class);
+        startActivity(intent);
+    }
+}
